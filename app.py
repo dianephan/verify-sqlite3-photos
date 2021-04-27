f4d48a1c413276360aba29eeaacd3e670d020df3
dIANE phan <dianephan12@gmail.com>
2021-04-26 18:35:47 -0700

    code to handle the whatsapp entries

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from twilio.base.exceptions import TwilioRestException
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 from image_classifier import get_tags
+from handle_blob_data import convert_into_binary, write_to_file, read_blob_data
 
 import sqlite3
 from sqlite3 import Error
@@ -54,7 +55,7 @@ def reply():
       insert_users = ''' INSERT INTO uploads(phone_number, latitude, longitude, file_name, file_blob)
         VALUES(?) '''
       cur = conn.cursor()
-      cur.execute(insert_users, (sender_phone_number, message_latitude, message_longitude, "UPDATEPIC", "UPDATEBLOB",))
+      cur.execute(insert_users, (sender_phone_number, message_latitude, message_longitude, "PIC URL HERE", "BLOB UNNECESSARY",))
       conn.commit()
       return respond(f'Thanks for sending in your location! Finish your entry by sending in a photo of the sky.')
     # if the user exists in the database and sends in a media message
@@ -64,51 +65,31 @@ def reply():
       cur.execute(look_up_user_query, [sender_phone_number]) 
       query_result = cur.fetchone()
       user_id = query_result[0]
-      # need function to create blob data here
-
-
-      update_user_picture = '''UPDATE uploads
-        SET file_name = ?
-        SET file_name, file_blob = ?
-        WHERE user_id = ?'''
-      cur = conn.cursor()
-      cur.execute(update_user_picture, (pic_url, user_id))
-      conn.commit()
-      return respond(f'You\'re all set!')
-
-
-
-
-
-  # ???????
-  if media_msg == '1' and sender in sky_pics:
-    pic_url = request.form.get('MediaUrl0')  # URL of the person's media
-    relevant_tags = get_tags(pic_url)
-    print("The tags for your picture are : ", relevant_tags)
-    # if 'sky' or 'weather' in relevant_tags and sky_pics.get(sender)[4] is None:
-    if 'sky' in relevant_tags:
-
-
-      sky_pics.get(sender)[4] = pic_url
-      print("[INFO] : sender has set their pic " , sky_pics.get(sender))
-      return respond(f'Thanks for sending in a picture.')
-      # if 'sky' or 'weather' in relevant_tags and sky_pics.get(sender)[4] is not None:
-      #     # replace the picture URL in sky_pics dictionary
-      #     sky_pics.get(sender)[4] = pic_url
-      #     print("[INFO] : sender has updated their pic " , sky_pics.get(sender))
-      #     return respond(f'Your picture has been updated.')
+      # need to check tags before adding to db
+      pic_url = request.form.get('MediaUrl0')  # URL of the person's media
+      relevant_tags = get_tags(pic_url)
+      print("The tags for your picture are : ", relevant_tags)
+      if 'sky' in relevant_tags:
+        update_user_picture = '''UPDATE uploads
+          SET file_name = ?
+          WHERE user_id = ?'''
+        print("[DATA] : ")
+        cur = conn.cursor()
+        cur.execute(update_user_picture, (pic_url, user_id))
+        conn.commit()
+        print("[INFO] : sender has set their pic ")
+        return respond(f'You\'re all set!')
       else:
         return respond(f'Please send in a picture of the sky.')
-  elif message_latitude is not None and message_longitude is not None:
-    location = get_location(message_latitude, message_longitude)
-    sky_pics[sender] = [None] * 5
-    sky_pics.get(sender)[0] = message_latitude
-    sky_pics.get(sender)[1] = message_longitude
-    sky_pics.get(sender)[2] = location[0]
-    sky_pics.get(sender)[3] = location[1]
-    return respond(f'Your location has been set to : {location}')
-  else:
-    return respond(f'Please send your current location, then send a picture of the sky.')
+    else:
+      return respond(f'Please send your current location, then send a picture of the sky.')
+  except Error as e:
+    print(e)
+  finally:
+    if conn:
+      conn.close()
+    else:
+      error = "how tf did u get here."
 
 # code for non-whatsapp users here
 @app.route('/', methods=['GET', 'POST'])
